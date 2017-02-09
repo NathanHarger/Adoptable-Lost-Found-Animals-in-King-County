d246d6721f468f27238e53cd7ce72995bcbbb0c2
Nathan Harger <nathan.j.harger@gmail.com>
2017-02-09 00:08:37 -0800

    All the markers are shown. Mistake with shoreline and redmond etc.

--- server.R
+++ server.R
@@ -13,6 +13,20 @@ library(jsonlite)
 source("www/helpers.R")
 
 aData <- getData()
+
+dogCountTable <-function()
+{
+  df <- table(aData$city)
+  df <- as.data.frame(df)
+  df[12,2] <- df[12,2] +df[13,2]
+ df[31,2] <- df[31,2] +df[32,2]
+  df <- df[-c(33,32, 23,22,13,1),]
+  df["lat"] <- c(47.282222,47.302222,47.6,47.771667,47.468333,47.648333,47.365833,47.734167,47.807869, 47.2025, 47.566389, 47.316667, 47.535556, 	47.752778, 	47.382778, 47.685833, 47.827778, 47.366111, 47.493889, 47.669444, 47.486667, 47.608889, 47.441389, 47.609722, 47.756389,	47.533056, 47.241389, 47.478333, 47.644167)
+  df["lng"] <-  c(-122.254167, -122.214722, -122.166667, -122.204444	,-122.345556,-121.908611,-122.100278,  -121.975556, -122.360133, -121.994167,-121.895,	-122.35, -122.043333, -122.247222,-122.226944,-122.191667, -122.305278,-122.044722, -121.786111,-122.123889, -122.195278,-122.042222, -122.293056,-122.333056, -122.339722, -121.844444, -122.459444, -122.275556, -122.216667		)
+  View(df)
+   return(df)
+  
+}
 #dataTable <-  table(aData$animal_breed,aData$city) 
 
 # Define server logic required to draw a histogram
@@ -31,11 +45,11 @@ shinyServer(function(input, output) {
   })
 
   output$mymap <- renderLeaflet({
-    leaflet(na.omit(aData)) %>%
+    leaflet(dogCountTable()) %>%
       addProviderTiles("Stamen.TonerLite",
                        options = providerTileOptions(noWrap = FALSE)
       ) %>%
-      addCircleMarkers( radius=10)%>%setView(-120, 46, zoom=8)
+      addCircleMarkers(popup=~as.character(Var1), radius=~Freq, stroke = FALSE, fillOpacity = 0.5)
   })
   
  
--- www/helpers.R
+++ www/helpers.R
@@ -13,3 +13,4 @@ getData <- function() {
 
 
 
+
