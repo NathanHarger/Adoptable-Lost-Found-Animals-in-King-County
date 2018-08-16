f823e6c2bc53a236fd60302657ec64c75104449b
Nathan Harger <nathan.j.harger@gmail.com>
2018-08-16 15:07:58 -0700

    Remove grouping of smooth/long hair chi and american pit bull/pit bull

--- server.R
+++ server.R
@@ -1,10 +1,12 @@
 
+
+
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
@@ -21,12 +23,16 @@ latList <- getLat()
 
 longList <- getLong()
 
-
-content <- function(city,freq)
+content <- function(city, freq)
 {
-  result <- paste( sep="<br/>",
-                   "<b><i>",city, "</i></b>", 
-                   getAnimalDistribution(city,freq),paste("<p> Total: ", freq, "</p>"))
+  result <- paste(
+    sep = "<br/>",
+    "<b><i>",
+    city,
+    "</i></b>",
+    getAnimalDistribution(city, freq),
+    paste("<p> Total: ", freq, "</p>")
+  )
   
   
   
@@ -34,33 +40,29 @@ content <- function(city,freq)
   return(result)
 }
 
-getAnimalDistribution <- function(city,freq)
-{  
-  animalTypeTable <- table(aData$city,aData$animal_type)
+getAnimalDistribution <- function(city, freq)
+{
+  animalTypeTable <- table(aData$city, aData$animal_type)
   
   row <- animalTypeTable[city,]
   result <- ""
   names <- colnames(row)
   for (cn in colnames(row))
   {
-    
-    
-    
-    result <- paste(result,cn, ":", " ", row[city,cn], "<br/>")
+    result <- paste(result, cn, ":", " ", row[city, cn], "<br/>")
     
   }
   return(result)
   
 }
 
-dogCountTable <-function()
+dogCountTable <- function()
 {
-  
   df <- table(aData$city)
   df <- as.data.frame(df)
   
   #Var1 is the city column
-  indecies <- which(tolower(cityList) %in% tolower(df$Var1) )
+  indecies <- which(tolower(cityList) %in% tolower(df$Var1))
   df <- subset(df, tolower(df$Var1) %in% tolower(cityList))
   
   
@@ -68,11 +70,11 @@ dogCountTable <-function()
   
   lat <- c()
   long <- c()
-  for(i in indecies)
+  for (i in indecies)
   {
-    lat[length(lat)+1] = latList[i]
+    lat[length(lat) + 1] = latList[i]
     
-    long[length(long)+1] = longList[i]
+    long[length(long) + 1] = longList[i]
     
     
   }
@@ -82,6 +84,7 @@ dogCountTable <-function()
   return(df)
   
 }
+
 ax <- list(
   title = "",
   zeroline = FALSE,
@@ -92,46 +95,25 @@ ax <- list(
 calculatePercentDog <- function()
 {
   df <- table(aData$animal_type)
-  loc <- grep("Dog",names(df), value=TRUE)
+  loc <- grep("Dog", names(df), value = TRUE)
   dogTotal <- 0
   
-  for(val in loc){
+  for (val in loc) {
     dogTotal = dogTotal + df[[val]]
   }
   total <- sum(df)
   
-  return(ceiling(dogTotal/total *100))
+  return(ceiling(dogTotal / total * 100))
 }
 
 breedSummary <- function()
 {
-  
-  df <-table(aData$animal_breed)
-  
-  df[["American Pit Bull Terrier"]] <- df[["American Pit Bull Terrier"]] + df[["Pit Bull"]]
-  
-  loc <- which(names(df) == "Pit Bull")
-  df <- df[-c(loc)]
-  
-  df["Chihuahua"] <- df[["Chihuahua - Long Haired"]] + df[["Chihuahua - Smooth Coated"]]
-  
-  
-  
-  loc <- which(names(df) %in% c("Chihuahua - Long Haired","Chihuahua - Smooth Coated" ))
-  
-  df <- df[-c(loc)]
-  
-  
+  df <- table(aData$animal_breed)
   
   return(df)
   
 }
-filterData <- function()
-{
-  #d<-aData[as.numeric(format(aData$date,'%Y')) == input$When,]
-  
-  return(aData)
-}
+
 citySummary <- function()
 {
   df <- table(aData$city)
@@ -140,43 +122,55 @@ citySummary <- function()
 }
 
 
-#dataTable <-  table(aData$animal_breed,aData$city) 
-
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
-  
   breedPieData <- breedSummary()
   output$breedPie <- renderPlot({
-    pie(breedPieData[breedPieData > 5], main="Top Breeds", col=rainbow(length(breedPieData)))
-    
-    
+    pie(tail(sort(breedPieData), 10), main = "Top 10 Breeds", col = rainbow(length(breedPieData)))
   })
   output$breedType <- flexdashboard::renderGauge({
-    gauge(calculatePercentDog(), min = 0, max = 100, symbol = '%', label = "Percent Dog",gaugeSectors(
-      success = c(100, 6), warning = c(5,1), danger = c(0, 1), colors = c("#CC6699")
-    ))
+    gauge(
+      calculatePercentDog(),
+      min = 0,
+      max = 100,
+      symbol = '%',
+      label = "Percent Dog",
+      gaugeSectors(
+        success = c(100, 6),
+        warning = c(5, 1),
+        danger = c(0, 1),
+        colors = c("#CC6699")
+      )
+    )
     
   })
+  print(dogCountTable())
+  
+  pal <- colorQuantile("Reds", dogCountTable()$Freq, n = 4)
+  cityAnimalDataFilter <- function(freq) {
+    print(freq)
+    r <- which(freq != 0)
+    return(r)
+  }
+  
   output$cityPie <- renderPlot({
-    
     data <- citySummary()
-    pie(data[data>5], main="Top Cities", col=rainbow(length(data))) 
+    pie(tail(sort(data), 5), main = "Top 5 Cities", col = rainbow(length(data)))
   })
   
   observe({
-    filteredData <- filterData()
     output$mymap <- renderLeaflet({
       data <- dogCountTable()
       leaflet(dogCountTable()) %>% addTiles() %>%
-        fitBounds(~min(lng),~min(lat),~max(lng), ~max(lat)) %>%
-        addCircles(popup=content(as.character(data$Var1), data$Freq), radius=  ~Freq*100, stroke = TRUE, weight=2, fillOpacity = 0.5)
+        fitBounds(~ min(lng),  ~ min(lat),  ~ max(lng), ~ max(lat)) %>%
+        addCircleMarkers(
+          popup = content(as.character(data$Var1[cityAnimalDataFilter(data$Var1)]), data$Freq[cityAnimalDataFilter(data$Freq)]),
+          stroke = FALSE,
+          fillOpacity = 0.5,
+          color = ~ pal(Freq)
+          
+        )
       
     })
   })
-  
 })
-
-
-
-
--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #absolutePanel
 
@@ -13,46 +13,58 @@ library(DT)
 library(plotly)
 library(flexdashboard)
 # Define UI for application that draws a histogram
-shinyUI(bootstrapPage(
-# Application title
-               tags$head(tags$style(".leaflet-control-zoom { display: none; }")),
-              tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-              tags$head(tags$style(
-                HTML('
-                     #sidebar {
-                      background-color: #FFFFFF;
-                      opacity: .85;
-                     }
-                     
-                     #copyright{
-                      font-size: x-small;
-                     }
-                   
-                    #title{background-color: #41f4bb;text-transform:capitalize; font-size: xx-large; text-align:center;}
-                     '
-         
-                )
-                )),
-              leafletOutput("mymap",width = "100%", height = "100%"),
-              
-
+shinyUI(
+  bootstrapPage(
+    # Application title
+    tags$head(tags$style(".leaflet-control-zoom { display: none; }")),
+    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+    tags$head(tags$style(
+      HTML(
+        '
+        #sidebar {
+        background-color: #FFFFFF;
+        opacity: .85;
+        }
+        
+        #copyright{
+        font-size: x-small;
+        }
+        
+        #title{background-color: #41f4bb;text-transform:capitalize; font-size: xx-large; text-align:center;}
+        '
+        
+      )
+    )),
+    leafletOutput("mymap", width = "100%", height = "100%"),
     
-    
-  absolutePanel( id="sidebar" ,
-                    top = "5%", left = "auto", right = 0, bottom = 0,width="30%", height = "95%",
-
-          plotOutput("cityPie"), flexdashboard::gaugeOutput("breedType"),plotOutput("breedPie",height='35%'),
-          div( "Data Provided by:" ,a(href="https://data.kingcounty.gov/Pets/Lost-found-adoptable-pets/yaai-7frk", em("Regional Animal Services of King County")), id = "copyright")
-          
-  ),
-  absolutePanel(id="title",top = 0, left = 0, right = "auto", bottom = 0,width="100%", height = "5%", div("Adoptable, Lost, Found Animals in King County") )
-  
-
-   
-  
-))
-
-  
-
-
-    
+    absolutePanel(
+      id = "sidebar" ,
+      top = "5%",
+      left = "auto",
+      right = 0,
+      bottom = 0,
+      width = "30%",
+      height = "95%",
+      
+      plotOutput("cityPie"),
+      plotOutput("breedPie", height = '35%'),
+      flexdashboard::gaugeOutput("breedType"),
+      div(
+        "Data Provided by:" ,
+        a(href = "https://data.kingcounty.gov/Pets/Lost-found-adoptable-pets/yaai-7frk", em("Regional Animal Services of King County")),
+        id = "copyright"
+      )
+      
+    ),
+    absolutePanel(
+      id = "title",
+      top = 0,
+      left = 0,
+      right = "auto",
+      bottom = 0,
+      width = "100%",
+      height = "5%",
+      div("Adoptable, Lost, Found Animals in King County")
+    )
+    )
+    )
